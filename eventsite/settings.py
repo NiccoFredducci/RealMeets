0b1cb8d17b129f342ed1ba831623111c963c5898
NiccoFredducci <niccolo.fredducci@edu.unifi.it>
2026-06-30 16:54:56 +0200

    Added whitenoise

--- eventsite/settings.py
+++ eventsite/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)4v8ejry-m283*jh)c8vbrm#suuk+j-bn=_&3!ikd!^yso)ao'
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-b)4v8ejry-m283*jh)c8vbrm#suuk+j-bn=_&3!ikd!^yso)ao")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
