b7fac9ff92493a76da7e4944f19ddf277b190ad4
NiccoFredducci <niccolo.fredducci@edu.unifi.it>
2026-06-30 17:36:24 +0200

    database attempt

--- eventsite/settings.py
+++ eventsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import dj_database_url
 import os
 
 from pathlib import Path
@@ -79,10 +80,9 @@ WSGI_APPLICATION = 'eventsite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
 }
 
 
