c0f093d272accfe7ece9449a2025762dfa32dea3
NiccoFredducci <niccolo.fredducci@edu.unifi.it>
2026-07-01 17:14:58 +0200

    Latest deployment test with cloudinary

--- create_superuser.py
+++ create_superuser.py
@@ -5,9 +5,49 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventsite.settings")
 django.setup()
 
 from django.contrib.auth.models import User
+from accounts.models import Profile  # adjust if needed
 
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin_demo",
-        password="admin12345"
+
+def create_user(username, password, role=None, is_superuser=False):
+    if User.objects.filter(username=username).exists():
+        return
+
+    user = User.objects.create_user(
+        username=username,
+        password=password
     )
+
+    if is_superuser:
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+    profile, created = Profile.objects.get_or_create(user=user)
+
+    if role:
+        profile.role = role
+        profile.save()
+
+    return user
+
+
+admin = create_user(
+    username="admin_demo",
+    password="admin12345",
+    role="manager",
+    is_superuser=True
+)
+
+create_user(
+    username="manager_demo",
+    password="manager12345",
+    role="manager",
+    is_superuser=False
+)
+
+create_user(
+    username="user_demo",
+    password="user12345",
+    role="subscriber",
+    is_superuser=False
+)
